3d8dc1143ffb357598940a5f57cd6923c8724665
hqh <kianwee@163.com>
2020-02-02 12:26:05 +0800

    test

---
+++ test.ts
@@ -0,0 +1,11 @@
+// tests go here; this will not be compiled when this package is used as an extension.
+basic.forever(() => { 
+
+    while (true)
+    {
+        //ZDR.ReadPin(0);
+        console.log("abcdefg");
+    }    
+  
+}
+)
